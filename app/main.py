e260ae42d10c0b18838cc4af8bfc949383f1c28e
ganthalerandreas <ganthaler.andreas@gmail.com>
2026-06-21 23:14:46 +0200

    Integrate WebSocket and Jinja2 templates
    Added WebSocket support and Jinja2 templating.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi import Request
+
+from app.ocpp_handler import ChargePointHandler
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 @app.get("/")
-def home():
-    return {"status": "ok"}
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.websocket("/ocpp/{cp_id}")
+async def ocpp_endpoint(websocket: WebSocket, cp_id: str):
+
+    await websocket.accept()
+
+    cp = ChargePointHandler(cp_id, websocket)
+
+    await cp.run()
