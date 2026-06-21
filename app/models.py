428888ad669f08e0fe69e64a5810ce45bf692f31
ganthalerandreas <ganthaler.andreas@gmail.com>
2026-06-21 23:41:39 +0200

    Add ChargingSession model for database

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class ChargingSession(Base):
+    __tablename__ = "sessions"
+
+    id = Column(Integer, primary_key=True)
+
+    start_time = Column(String)
+    end_time = Column(String)
+
+    meter_start = Column(Float)
+    meter_stop = Column(Float)
+
+    energy_kwh = Column(Float)
+    total_euro = Column(Float)
+
+    status = Column(String)
