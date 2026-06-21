fa88b20dd8b8df329d2e758bce824fb4ba847a45
ganthalerandreas <ganthaler.andreas@gmail.com>
2026-06-22 00:16:36 +0200

    Refactor imports and clean up formatting in main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,20 @@
-import os
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-from app.ocpp_handler import ChargePointHandler
+import os
 
 app = FastAPI()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-templates = Jinja2Templates(
-    directory=os.path.join(BASE_DIR, "../templates")
-)
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "../templates"))
 
 app.mount(
     "/static",
     StaticFiles(directory=os.path.join(BASE_DIR, "../static")),
     name="static"
 )
-
 
 @app.get("/")
 def home(request: Request):
