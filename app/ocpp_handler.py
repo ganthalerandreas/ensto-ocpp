f29c64e8ab8d4436bda229a778560ce52b5a03fb
ganthalerandreas <ganthaler.andreas@gmail.com>
2026-06-21 23:42:39 +0200

    Implement charging session management in OCPP handler

--- app/ocpp_handler.py
+++ app/ocpp_handler.py
@@ -1,8 +1,14 @@
 from ocpp.v16 import ChargePoint as CP
 from ocpp.v16 import call_result
 from ocpp.routing import on
+
+from app.database import SessionLocal
+from app.models import ChargingSession
+
 import datetime
 
+
+PRICE_PER_KWH = 0.30  # modificabile
 
 class ChargePointHandler(CP):
 
@@ -12,31 +18,45 @@ class ChargePointHandler(CP):
     @on("BootNotification")
     async def boot(self, **kwargs):
 
-        print("Wallbox connessa")
-
         return call_result.BootNotification(
             current_time=datetime.datetime.utcnow().isoformat(),
             interval=30,
             status="Accepted"
         )
 
-    @on("StatusNotification")
-    async def status(self, **kwargs):
-
-        print("Status ricevuto")
-
-        return call_result.StatusNotification()
-
     @on("StartTransaction")
-    async def start(self, **kwargs):
+    async def start(self, meter_start, timestamp, **kwargs):
 
-        print("Ricarica iniziata")
+        db = SessionLocal()
 
-        return call_result.StartTransaction(transaction_id=1)
+        session = ChargingSession(
+            start_time=timestamp,
+            meter_start=meter_start,
+            status="Charging"
+        )
+
+        db.add(session)
+        db.commit()
+
+        return call_result.StartTransaction(transaction_id=session.id)
 
     @on("StopTransaction")
-    async def stop(self, **kwargs):
+    async def stop(self, transaction_id, meter_stop, timestamp, **kwargs):
 
-        print("Ricarica terminata")
+        db = SessionLocal()
+
+        session = db.query(ChargingSession).filter_by(id=transaction_id).first()
+
+        session.end_time = timestamp
+        session.meter_stop = meter_stop
+
+        kwh = (meter_stop - session.meter_start) / 1000
+        session.energy_kwh = kwh
+        session.total_euro = kwh * PRICE_PER_KWH
+        session.status = "Completed"
+
+        db.commit()
+
+        print("RICARICA COMPLETATA:", session.total_euro)
 
         return call_result.StopTransaction()
