7a9f4943047bb231e92ce111f86eca096f724230
ganthalerandreas <ganthaler.andreas@gmail.com>
2026-06-22 15:33:27 +0200

    Refactor WebSocket handling and improve connection logic

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+import asyncio
+import datetime
+import websockets
+
 from ocpp.v16 import ChargePoint as CP
 from ocpp.v16 import call_result
 from ocpp.routing import on
-import datetime
 
 app = FastAPI()
 
@@ -16,6 +19,7 @@ class ChargePointHandler(CP):
     async def on_boot(self, **kwargs):
 
         global current_status
+
         current_status = "Connessa"
 
         print("BootNotification ricevuto")
@@ -26,59 +30,61 @@ class ChargePointHandler(CP):
             status="Accepted"
         )
 
+
     @on("StatusNotification")
     async def on_status(self, status, **kwargs):
 
         global current_status
+
         current_status = status
 
         print("STATUS:", status)
 
-        return call_result.StatusNotification()
+
+async def on_connect(websocket):
+
+    global current_status
+
+    if websocket.subprotocol != "ocpp1.6":
+        print("Protocollo errato:", websocket.subprotocol)
+
+        await websocket.close()
+
+        return
+
+    cp_id="ENSTO001"
+
+    cp=ChargePointHandler(
+        cp_id,
+        websocket
+    )
+
+    print("Wallbox collegata")
+
+    await cp.start()
+
+
+@app.on_event("startup")
+async def startup():
+
+    asyncio.create_task(
+        websockets.serve(
+            on_connect,
+            "0.0.0.0",
+            9000,
+            subprotocols=["ocpp1.6"]
+        )
+    )
 
 
 @app.get("/")
 def home():
 
     return HTMLResponse(f"""
-    <!DOCTYPE html>
     <html>
-    <head>
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    </head>
     <body>
     <h1>Wallbox Status</h1>
     <h2>{current_status}</h2>
     </body>
     </html>
     """)
-
-
-@app.websocket("/ENSTO001")
-async def websocket_endpoint(websocket: WebSocket):
-
-    protocols = websocket.headers.get(
-        "sec-websocket-protocol"
-    )
-
-    if protocols != "ocpp1.6":
-        print("Subprotocol errato:", protocols)
-        await websocket.close()
-        return
-
-    await websocket.accept(
-        subprotocol="ocpp1.6"
-    )
-
-    print("Wallbox collegata")
-
-    cp = ChargePointHandler(
-        "ENSTO001",
-        websocket
-    )
-
-    try:
-        await cp.start()
-
-    except WebSocketDisconnect:
-        print("Wallbox disconnessa")
